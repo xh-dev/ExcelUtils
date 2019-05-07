b9ccd93f934a1395850e6bb7d344d1c2e3fb21b6
DESKTOP-MM3VFOH\Xeth-Surface-Pro <adamhung12@gmail.com>
2019-05-07 23:31:23 +0800

    complete style issue

--- src/test/sources/me/xethh/util/excelUtils/AppTest.java
+++ src/test/sources/me/xethh/util/excelUtils/AppTest.java
@@ -37,20 +37,18 @@ public class AppTest
         }
         return is;
     }
+    public static byte[] toByteArr(String[] ss){
+        int[] is = toIntArr(ss);
+        byte[] ba = new byte[ss.length];
+        for(int i=0;i<is.length;i++){
+            ba[i] = (byte) is[i];
+        }
+        return ba;
+    }
     public static void main(String[] args) throws IOException {
         InputStream is = new FileInputStream(new File("./src/test/resources/TestingBase.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(is);
-        // Iterator<CellScanningModel> scanning = WorkbookScanning.scan(workbook);
-        // while(scanning.hasNext()){
-        //     System.out.println(scanning.next());
-        // }
         Iterator<String[]> scanning = WorkbookScanning.scanAsArr(workbook);
-        // while(scanning.hasNext()){
-        //     for(String s:scanning.next())
-        //         System.out.print(s+", ");
-        //     System.out.println();
-        // }
-        // FileOutputStream os = new FileOutputStream(new File("./src/test/resources/TestingBase_revised.xlsx"));
         XSSFWorkbook wb = new XSSFWorkbook();
         XSSFSheet sheet = wb.createSheet("abc");
         int row = 0;
@@ -58,48 +56,55 @@ public class AppTest
             String[] rowRecord = scanning.next();
             XSSFRow sheetRow = sheet.createRow(row);
             for(int i=0;i<rowRecord.length;i++) {
-                sheetRow.createCell(i).setCellValue(rowRecord[i]);
-            }
-            if(rowRecord[27]!=null && !rowRecord[27].equals("") && rowRecord[27].split(",").length==3){
-                int[] fontColor = toIntArr(rowRecord[27].split(","));
-                int sum = 0;
-                for(int s : fontColor)
-                    sum+=s;
-                if(sum>0){
-                    XSSFCellStyle style = sheetRow.getCell(27).getCellStyle();
-                    style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-                    style.setFillForegroundColor(new XSSFColor(new java.awt.Color(fontColor[0],fontColor[1],fontColor[2]),wb.getStylesSource().getIndexedColors()));
-                    sheetRow.getCell(27).setCellStyle(style);
+                XSSFCell cell = sheetRow.createCell(i);
+                if(i==27 && rowRecord[i]!=null && !rowRecord[i].equals("") && rowRecord[i].split(",").length==3){
+                    int[] fontColor = toIntArr(rowRecord[i].split(","));
+                    byte[] fontColorByte = toByteArr(rowRecord[i].split(","));
+                    int sum = 0;
+                    for(int s : fontColor)
+                        sum+=s;
+                    if(sum>0){
+                        XSSFCellStyle style = wb.createCellStyle();
+                        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                        style.setFillForegroundColor(new XSSFColor(fontColorByte,wb.getStylesSource().getIndexedColors()));
+                        cell.setCellStyle(style);
+                    }
                 }
-            }
-            if(rowRecord[8]!=null && !rowRecord[8].equals("") && rowRecord[8].split(",").length==4){
-                int[] fontColor = toIntArr(rowRecord[8].split(","));
-                int sum = 0;
-                for(int s : fontColor)
-                    sum+=s;
-                if(sum>0){
-                    XSSFCellStyle style = sheetRow.getCell(8).getCellStyle();
-                    style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-                    style.setFillForegroundColor(new XSSFColor(new java.awt.Color(fontColor[0],fontColor[1],fontColor[2],fontColor[3]),wb.getStylesSource().getIndexedColors()));
-                    sheetRow.getCell(8).setCellStyle(style);
+                if(i==8 && rowRecord[8]!=null && !rowRecord[8].equals("") && rowRecord[8].split(",").length==4){
+                    int[] fontColor = toIntArr(rowRecord[8].split(","));
+                    byte[] fontColorByte = toByteArr(rowRecord[i].split(","));
+                    int sum = 0;
+                    for(int s : fontColor)
+                        sum+=s;
+                    if(sum>0){
+                        XSSFCellStyle style = wb.createCellStyle();
+                        sheetRow.getCell(12).getCellStyle();
+                        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                        style.setFillForegroundColor(new XSSFColor(fontColorByte,wb.getStylesSource().getIndexedColors()));
+                        sheetRow.getCell(8).setCellStyle(style);
+                    }
                 }
-            }
-            if(rowRecord[19]!=null && !rowRecord[19].equals("") && rowRecord[19].split(",").length==4){
-                int[] fontColor = toIntArr(rowRecord[19].split(","));
-                int sum = 0;
-                for(int s : fontColor)
-                    sum+=s;
-                if(sum>0){
-                    XSSFCellStyle style = sheetRow.getCell(19).getCellStyle();
-                    style.setFillForegroundColor(new XSSFColor(new java.awt.Color(fontColor[0],fontColor[1],fontColor[2],fontColor[3]),wb.getStylesSource().getIndexedColors()));
-                    style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-                    sheetRow.getCell(19).setCellStyle(style);
+                if(i == 19 && rowRecord[19]!=null && !rowRecord[19].equals("") && rowRecord[19].split(",").length==4){
+                    int[] fontColor = toIntArr(rowRecord[19].split(","));
+                    byte[] fontColorByte = toByteArr(rowRecord[i].split(","));
+                    int sum = 0;
+                    for(int s : fontColor)
+                        sum+=s;
+                    if(sum>0){
+                        XSSFCellStyle style = wb.createCellStyle();
+                        style.setFillForegroundColor(new XSSFColor(fontColorByte,wb.getStylesSource().getIndexedColors()));
+                        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                        sheetRow.getCell(19).setCellStyle(style);
+                    }
                 }
+                cell.setCellValue(rowRecord[i]);
             }
             row++;
         }
         try {
-            FileOutputStream outputStream = new FileOutputStream("./target/TestingBase_revised.xlsx");
+            String filePath = "./target/TestingBase_revised.xlsx";
+            if(new File(filePath).exists()) new File(filePath).delete();
+            FileOutputStream outputStream = new FileOutputStream(filePath);
             wb.write(outputStream);
             wb.close();
         } catch (FileNotFoundException e) {
